ed94a22cef7ba7d6de0aa488c28b85bd3d3e6e0e
Leo <theexpendablesgames@gmail.com>
2023-10-24 20:31:28 -0300

    First commit

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -5,13 +5,8 @@ import { Container } from "./styles";
 export default function Main() {
   return (
     <Container>
-      <img
-        src="https://devsamurai-materials.s3.amazonaws.com/templates/avatar-gradient-dark.svg"
-        height="256"
-        alt="Dev Samurai"
-      />
-      <h1>Fala Samurai!</h1>
-      <p>Esse é o template typescript básico da Dev Samurai para React.</p>
+      Teste
+
     </Container>
   );
 }
